a84ee807cf4a5a822f330962a46b5d9a17e87947
orsys <contact@orsys.fr>
2023-02-06 13:45:31 +0100

    Build Steps

--- projects/klg/src/lib/klg.module.ts
+++ projects/klg/src/lib/klg.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { KlgComponent } from './klg.component';
-
 
 
 @NgModule({
   declarations: [
-    KlgComponent
   ],
   imports: [
   ],
   exports: [
-    KlgComponent
   ]
 })
 export class KlgModule { }
--- projects/klg/src/public-api.ts
+++ projects/klg/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of klg
  */
 
-export * from './lib/klg.service';
-export * from './lib/klg.component';
 export * from './lib/klg.module';
