dc53e7f053ff6bf3206288efee605187bd3c41ba
orsys <contact@orsys.fr>
2023-02-07 15:59:03 +0100

    Sub Component

--- projects/klg/src/lib/card/card.module.ts
+++ projects/klg/src/lib/card/card.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CardComponent } from './card.component';
 import { NgImportModule } from '../ng-import.module';
 import { ButtonModule } from '../button/button.module';
+import { CardTitleComponent } from './card-title.component';
+import { CardContentComponent } from './card-content.component';
 
 
 
 @NgModule({
   declarations: [
-    CardComponent
+    CardComponent,
+    CardTitleComponent,
+    CardContentComponent
   ],
   imports: [
     NgImportModule,
     ButtonModule
   ],
   exports: [
-    CardComponent
+    CardComponent,
+    CardTitleComponent,
+    CardContentComponent
   ]
 })
 export class CardModule { }
