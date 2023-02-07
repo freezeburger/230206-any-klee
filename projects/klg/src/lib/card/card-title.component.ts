baa248d0ca90f32e1ca1fd6f4b103c1890a1d949
orsys <contact@orsys.fr>
2023-02-07 16:45:20 +0100

    Update Host Control

--- projects/klg/src/lib/card/card-title.component.ts
+++ projects/klg/src/lib/card/card-title.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Host, Optional } from '@angular/core';
+import { CardComponent } from './card.component';
 
 @Component({
   selector: 'klg-card-title',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class CardTitleComponent {
+
+  constructor( @Optional() @Host() card:CardComponent){
+    if(!card) throw Error(' "CardTitleComponent" must be used in a "CardComponent" ');
+  }
+
 }
