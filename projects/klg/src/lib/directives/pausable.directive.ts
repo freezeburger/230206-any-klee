fcd3535b8804d9fdfa59fa802ac2a553de823ce7
orsys <contact@orsys.fr>
2023-02-08 11:40:00 +0100

    Renderer

--- projects/klg/src/lib/directives/pausable.directive.ts
+++ projects/klg/src/lib/directives/pausable.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[klgPausable]'
 })
 export class PausableDirective {
 
-  constructor() { }
+  pause = false;
+  layer:any = this.renderer.createElement('div');
+
+  constructor(
+    private renderer:Renderer2
+  ) { }
+
+  ngOnInit(){
+    console.warn(this.layer)
+    this.renderer.setStyle(this.layer, 'background-color', 'rgba(0, 0, 0, 0.5)');
+    this.renderer.setStyle(this.layer, 'position', 'absolute');
+    this.renderer.setStyle(this.layer, 'top', '0');
+    this.renderer.setStyle(this.layer, 'width', '100vw');
+    this.renderer.setStyle(this.layer, 'height', '100vh');
+    this.renderer.setStyle(this.layer, 'z-index', '1000');
+  }
+
+  @HostListener('window:keydown.alt.p')
+  togglePause(){
+    this.pause = ! this.pause;
+    this.updatePauseLayer();
+  }
+
+  updatePauseLayer(){
+    if(this.pause){
+      this.renderer.appendChild(document.body,this.layer);
+    }else{
+      this.renderer.removeChild(document.body,this.layer); 
+    }
+  }
 
 }
